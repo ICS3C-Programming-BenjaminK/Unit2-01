60ad7af25ad5859f896bc8355d87dff9d52c3dc1
Benjamin Kamanda <benjamin.kamanda@stu.ocsb.ca>
2025-02-28 17:22:19 +0000

    program to calculate length and width

---
+++ area_per_rect_input.py
@@ -0,0 +1,14 @@
+def main():
+
+    # area
+    length = float(input("Give a length: "))
+    width = float(input("Give a width: "))
+    print("The area is:")
+    print(length * width)
+
+    print("The perimeter is")
+    print ((length + width) * 2 )
+
+
+if __name__ == "__main__":
+    main()
